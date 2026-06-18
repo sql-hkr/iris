d1dfe15efd578e7d5130b2292cf12ad4cda1f750
sql-hkr <sql.hkr@gmail.com>
2026-06-19 00:24:57 +0900

    feat: add furo

--- source/conf.py
+++ source/conf.py
@@ -14,15 +14,25 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.mathjax"]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+mathjax3_config = {
+    "loader": {"load": ["[tex]/physics"]},
+    "tex": {
+        "packages": {"[+]": ["physics"]},
+        "macros": {
+            "argmax": r"\operatorname*{arg\,max}",
+            "argmin": r"\operatorname*{arg\,min}",
+        },
+    },
+}
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
